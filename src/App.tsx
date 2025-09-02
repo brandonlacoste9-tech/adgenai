2f87c9bb971c486c96765791f0417a01087edd13
brandonlacoste9-tech <brandonlacoste9@gmail.com>
2025-09-02 17:57:45 -0400

    Resolve App.tsx merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -16,13 +16,7 @@ import { AgencyPartnerOnePager } from './components/AgencyPartnerOnePager';
 import { AgencyManagementDashboard } from './components/AgencyManagementDashboard';
 import { CMSDashboard } from './components/CMSDashboard';
 import { PerformancePredictionDemo } from './components/PerformancePredictionDemo';
-import { ViralGrowthEngine } from './components/ViralGrowthEngine';
-import { CommunityHub } from './components/CommunityHub';
-import { AdvancedMLDashboard } from './components/AdvancedMLDashboard';
-import { ShareSummary } from './pages/ShareSummary';
-import { ShareChangelog } from './pages/ShareChangelog';
 import { SharePressKit } from './pages/SharePressKit';
-import { BrandGuidelines } from './components/BrandGuidelines';
 import { Footer } from './components/Footer';
 
 const HomePage: React.FC = () => (
@@ -55,13 +49,7 @@ function App() {
             <Route path="/cms" element={<CMSDashboard />} />
             <Route path="/analytics" element={<AnalyticsDashboard />} />
             <Route path="/demo" element={<div className="min-h-screen bg-gray-50 pt-20 py-12"><PerformancePredictionDemo /></div>} />
-            <Route path="/viral" element={<div className="min-h-screen bg-gray-50 pt-20 py-12"><ViralGrowthEngine /></div>} />
-            <Route path="/community" element={<CommunityHub />} />
-            <Route path="/ml-dashboard" element={<AdvancedMLDashboard />} />
-            <Route path="/share/adgenai" element={<div className="min-h-screen"><ShareSummary /></div>} />
-            <Route path="/share/changelog" element={<div className="min-h-screen"><ShareChangelog /></div>} />
             <Route path="/share/press-kit" element={<div className="min-h-screen"><SharePressKit /></div>} />
-            <Route path="/brand" element={<div className="min-h-screen bg-gray-50 pt-20 py-12"><div className="max-w-6xl mx-auto px-4"><BrandGuidelines /></div></div>} />
           </Routes>
         </main>
         <Footer />
